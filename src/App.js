088f8754640b143b4cd6a814966933ae1779dd8b
juhi-coder <kumarijuhimdb2@gmail.com>
2023-04-09 08:51:53 +0530

    task12

--- src/App.js
+++ src/App.js
@@ -1,100 +1,51 @@
-import "./App.css";
-import React, { useCallback, useEffect, useState } from "react";
-import MovieList from "./components/MovieList";
-import AddMovie from "./components/AddMovie";
-const DB_URL =
-  "https://ekart-website-a0b79-default-rtdb.firebaseio.com/movies.json";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ContactUs from './components/ContactUs';
+import About from './components/About';
+
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const movieHandler = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(DB_URL);
-
-      if (!response.ok) {
-        throw new Error("Something went wrong ....Retrying");
-      }
-      const data = await response.json();
-
-      const loadMovies = [];
-      for (const key in data) {
-        loadMovies.push({
-          id: key,
-          title: data[key].title,
-          openingText: data[key].openingText,
-          releaseDate: data[key].releaseDate,
-        });
-      }
-
-      setMovies(loadMovies);
-    } catch (error) {
-      setError(error.message);
-    }
-    setIsLoading(false);
-  }, []);
-
-  useEffect(() => {
-    movieHandler();
-  }, [movieHandler]);
-
-  async function AddMovieHandler(movie) {
-    const response = await fetch(
-      DB_URL,
-
-      {
-        method: "POST",
-        body: JSON.stringify(movie),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-  }
-
-  const deleteMovieHandler = async (id) => {
-    const response = await fetch(`DB_URL ${id}`, {
-      method: "DELETE",
-      body: JSON.stringify(id),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    movieHandler();
-    const data = await response.json();
-    console.log(data);
-  };
-
-  let content = <p>found no movies</p>;
-
-  if (movies.length > 0) {
-    content = <MovieList  movies={movies} onDeleteMovie={deleteMovieHandler} ></MovieList>;
-    console.log(movies);
-  }
-  if (error) {
-    content = <p>{error}</p>;
-  }
-  if (isLoading) {
-    content = <p>Loading..........</p>;
-  }
-
   return (
-   <div>
-      <section>
-        <AddMovie onAddMovie={AddMovieHandler}></AddMovie>
-      </section>
-      <section>
-        <button onClick={movieHandler}>Fetch Movies</button>
-      </section>
-      <section>{content}</section>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
