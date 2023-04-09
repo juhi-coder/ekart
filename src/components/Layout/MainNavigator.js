be19e8f4c94e235016d34e312bf7582c0d75f9f5
juhi-coder <kumarijuhimdb2@gmail.com>
2023-04-09 11:52:56 +0530

    task18

--- src/components/Layout/MainNavigator.js
+++ src/components/Layout/MainNavigator.js
@@ -1,8 +1,19 @@
 import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import {useHistory } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
 
 import classes from './MainNavigator.module.css';
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+  const history = useHistory();
+  const logoutHandler = () => {
+    authCtx.logout();
+
+    history.replace("/auth");
+  };
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -19,6 +30,21 @@ const MainNavigation = () => {
           <li>
             <button>Logout</button>
           </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
